8b4e122ea20d0b2f661ded9b76edd6d268204c51
L3MON4D3 <Simljk@outlook.de>
2022-08-17 19:40:26 +0200

    Allow customizing override-function.

--- lua/luasnip/extras/override_fn.lua
+++ lua/luasnip/extras/override_fn.lua
@@ -3,32 +3,40 @@ local snippet = require("luasnip").snippet
 
 local M = {}
 
+local function default_override(arg, override)
+	arg = arg or {}
+	override = override or {}
+
+	return vim.tbl_extend("keep", arg, override)
+end
+
+---Override keys in table-arguments of `fn`.
+---@param fn any function.
+---@vararg Any number of `override_descriptions`:
+--- These are tables with keys:
+---  - arg_indx: the position of the argument that should be extended
+---  - apply_override: fn(direct_arg, override_arg) -> arg. This function is
+---    responsible for overriding the arg passed to this specific call with the
+---    one provided earlier.
+---@return function: fn(...). The varargs are a list of values passed to
+--- `apply_override`.
 local function override_any(fn, ...)
-	local override_arg_indices = {...}
+	local override_descriptions = {...}
 
 	return function(...)
 		local override_arg_values = {...}
-
-		-- insert missing override-tables.
-		-- Prevents `or {}` in vim.tbl_extend, a few lines down.
-		for i, _ in ipairs(override_arg_indices) do
-			if not override_arg_values[i] then
-				override_arg_values[i] = {}
-			end
-		end
 
 		return function(...)
 			local direct_args = {...}
 
 			-- override values of direct argument.
-			for i, arg_indx in ipairs(override_arg_indices) do
-				direct_args[arg_indx] = direct_args[arg_indx] or {}
+			for override_indx, od in ipairs(override_descriptions) do
+				local arg_indx = od.arg_indx
 
 				-- still allow overriding with directly-passed keys.
-				-- Maybe we should even override in-place here.
-				-- Don't do it for now.
-				direct_args[arg_indx] = vim.tbl_extend("keep", direct_args[arg_indx], override_arg_values[i])
+				direct_args[arg_indx] = od.apply_override(direct_args[arg_indx], override_arg_values[override_indx])
 			end
+
 
 			-- important: http://www.lua.org/manual/5.3/manual.html#3.4
 			-- Passing arguments after the results from `unpack` would mess all this
@@ -39,6 +47,20 @@ local function override_any(fn, ...)
 end
 
 
-M.fmt = override_any(fmt, 3)
+M.fmt = override_any(
+	fmt,
+	{arg_indx = 3, apply_override = default_override} )
+
+local function context_override(arg, override)
+	if type(arg) == "string" then
+		arg = {trig = arg}
+	end
+	-- both are table or nil now.
+	return default_override(arg, override)
+end
+M.snippet = override_any(
+	snippet,
+	{arg_indx = 1, apply_override = context_override},
+	{arg_indx = 3, apply_override = default_override} )
 
 return M
